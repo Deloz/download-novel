4f9fb6bce33da60c50f745e4d65cd7d28397a2c2
Deloz <bugkmsor@gmail.com>
2015-06-20 00:03:22 +0800

    support lewen8.com

--- main.go
+++ main.go
@@ -4,21 +4,30 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"path"
 
 	"bitbucket.org/deloz/zilang/sites"
+	"bitbucket.org/deloz/zilang/utils"
 )
 
 func main() {
+	rootDir, err := os.Getwd()
+	utils.CheckError(err)
+	downloadPath := path.Join(rootDir, "download")
+
 	site := flag.String("site", "", "novel from which site")
 	listURL := flag.String("url", "", "the list url")
+	flag.Parse()
 	if len(*listURL) == 0 {
 		printHelp()
 	}
-	flag.Parse()
 
 	switch *site {
 	case "zilang":
-		sites.Zilang{}.ParseNovelList(*listURL)
+		sites.Zilang{}.ParseNovelList(*listURL, downloadPath)
+		break
+	case "lewen8":
+		sites.Lewen8{}.ParseNovelList(*listURL, downloadPath)
 		break
 	default:
 		printHelp()
@@ -27,8 +36,8 @@ func main() {
 
 func printHelp() {
 	fmt.Println(`
-	Usage examle => downloader -site=zilang -url=http://www.zilang.net/170/170694/
-	Now support sites :  zilang
+	Usage examle => novel-downloader -site=zilang -url=http://www.zilang.net/170/170694/
+	Now support sites :  zilang lewen8
 	`)
 	os.Exit(1)
 }
--- sites/lewen8.go
+++ sites/lewen8.go
@@ -7,24 +7,22 @@ import (
 	"regexp"
 	"strings"
 
-	"bitbucket.org/deloz/zilang/utils"
-
 	"github.com/PuerkitoBio/goquery"
+
+	"bitbucket.org/deloz/zilang/utils"
 )
 
-type Zilang struct{}
+type Lewen8 struct{}
 
-func (z Zilang) ParseNovelList(listURL, downloadPath string) {
+func (l Lewen8) ParseNovelList(listURL, downloadPath string) {
 	if !strings.HasPrefix(listURL, "/") {
 		listURL += "/"
 	}
-	doc, err := utils.FetchPage("gbk", listURL)
+	doc, err := utils.FetchPage("utf-8", listURL)
 	utils.CheckError(err)
 
-	re := regexp.MustCompile(`《|》`)
-	bookName := doc.Find(".book h1").Text()
-	bookName = re.ReplaceAllString(bookName, "")
-	author := doc.Find(".book .small span").First().Text()
+	bookName := doc.Find(".kfml .infot h1").Text()
+	author := strings.Replace(doc.Find(".kfml .infot span").Text(), "/著", "", -1)
 
 	filename := bookName + "--" + author + ".txt"
 
@@ -34,7 +32,7 @@ func (z Zilang) ParseNovelList(listURL, downloadPath string) {
 	_, err = f.WriteString(bookName + "\n" + author + "\n\n\n\n")
 	utils.CheckError(err)
 
-	doc.Find(".book .list ul li").Each(func(i int, s *goquery.Selection) {
+	doc.Find("#defaulthtml dd").Each(func(i int, s *goquery.Selection) {
 		title := s.Text()
 		url := s.Find("a").AttrOr("href", "")
 		if len(url) == 0 {
@@ -45,24 +43,26 @@ func (z Zilang) ParseNovelList(listURL, downloadPath string) {
 
 		log.Println("title: ", title)
 		log.Println("url: ", url)
-		content := z.downloadArticle(title, url)
+
+		content := l.downloadArticle(title, url)
 		_, err := f.WriteString(strings.TrimSpace(title) + "\n\n" + strings.TrimSpace(content) + "\n\n\n\n")
 		utils.CheckError(err)
 	})
 }
 
-func (z Zilang) downloadArticle(title, articleURL string) string {
-	defer utils.Un(utils.Trace("download zilang article: " + title + " url=>> " + articleURL))
-	doc, err := utils.FetchPage("gbk", articleURL)
+func (l Lewen8) downloadArticle(title, articleURL string) string {
+	defer utils.Un(utils.Trace("download lewen8 article: " + title + " url=>> " + articleURL))
+	doc, err := utils.FetchPage("utf-8", articleURL)
 	utils.CheckError(err)
 
-	html, err := doc.Find("#chapter_content").Html()
+	html, err := doc.Find("#content").Html()
 	utils.CheckError(err)
 	html = strings.TrimSpace(html)
-	re := regexp.MustCompile(`</p>\s*<p[^>]*>|<br\s*/?>`)
+	re := regexp.MustCompile(`</p>\s*<p[^>]*>|<br\s*/?>\s*<br\s*/?>|<br\s*/?>`)
 	html = re.ReplaceAllString(html, "\n")
-	re = regexp.MustCompile(`<!--.*?-->|<script[^>]*>.*?</script>|<a[^>]*>.*?</a>|<div[^>]*>|</div>|<p[^>]*>|</p>|\(紫琅文学http://www\.zilang\.net\)`)
+	re = regexp.MustCompile(`<!--.*?-->|<script[^>]*>.*?</script>|<a[^>]*>.*?</a>|<div[^>]*>|</div>|<p[^>]*>|</p>|&nbsp;|@@`)
 	html = re.ReplaceAllString(html, "")
+	html = strings.Replace(html, "\xEF\xBB\xBF", "", -1)
 
 	return html
 }
